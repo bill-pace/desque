f725acef682e2f98cd652ffac61309ab8f9a13c7
Bill Pace <billpace24@gmail.com>
2025-01-05 17:00:46 -0600

    implement std::error::Error for crate::error::Error

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,15 @@
+#[derive(Debug)]
 pub enum Error {
     BackInTime,
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let descriptor = match self {
+            Self::BackInTime => "event execution time is less than current simulation time",
+        };
+        write!(f, "{}", descriptor)
+    }
+}
+
+impl std::error::Error for Error {}
