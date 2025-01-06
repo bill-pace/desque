6e9b6d04ddb57a60d1bf8263b65e8ade6c4d484a
Bill Pace <billpace24@gmail.com>
2025-01-05 18:38:07 -0600

    add unit tests to make sure the simulation runs as expected

--- src/simulation.rs
+++ src/simulation.rs
@@ -4,7 +4,7 @@ use crate::EventQueue;
 pub struct Simulation<SimState, Time>
 where
     SimState: State,
-    Time: Ord + Clone
+    Time: Ord
 {
     pub event_queue: EventQueue<SimState, Time>,
     pub state: SimState,
@@ -13,7 +13,7 @@ where
 impl<SimState, Time> Simulation<SimState, Time>
 where
     SimState: State,
-    Time: Ord + Clone
+    Time: Ord
 {
     pub fn new(initial_state: SimState, start_time: Time) -> Self {
         Self {
@@ -32,5 +32,84 @@ where
             let mut next_event = next_event.unwrap();
             next_event.execute(&mut self.state, &mut self.event_queue);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Event;
+    use super::*;
+
+    #[derive(Eq, PartialEq, Ord, PartialOrd)]
+    struct SimTime {
+        time: u32,
+    }
+
+    struct SimState {
+        executed_event_values: Vec<u32>,
+        complete: bool
+    }
+    impl State for SimState {
+        fn is_complete(&self) -> bool {
+            self.complete
+        }
+    }
+
+    struct TestEvent {
+        value: u32,
+    }
+
+    impl Event<SimState, SimTime> for TestEvent {
+        fn execute(&mut self, simulation_state: &mut SimState, _: &mut EventQueue<SimState, SimTime>) {
+            simulation_state.executed_event_values.push(self.value);
+        }
+    }
+
+    struct CompletionEvent {}
+
+    impl Event<SimState, SimTime> for CompletionEvent {
+        fn execute(&mut self, simulation_state: &mut SimState, _: &mut EventQueue<SimState, SimTime>) {
+            simulation_state.complete = true;
+        }
+    }
+
+    fn setup() -> Simulation<SimState, SimTime> {
+        let mut sim = Simulation::new(
+            SimState {
+                executed_event_values: Vec::with_capacity(3),
+                complete: false,
+            },
+            SimTime { time: 0 },
+        );
+
+        let events: [TestEvent; 3] = [
+            TestEvent { value: 1 },
+            TestEvent { value: 3 },
+            TestEvent { value: 2 },
+        ];
+
+        for (i, event) in events.into_iter().enumerate() {
+            sim.event_queue.schedule(event, SimTime { time: 2 * i as u32 }).unwrap();
+        }
+        sim
+    }
+
+    #[test]
+    fn simulation_executes_events() {
+        let mut sim = setup();
+        sim.run();
+
+        let expected = vec![1, 3, 2];
+        assert_eq!(expected, sim.state.executed_event_values, "events did not execute in correct order");
+    }
+
+    #[test]
+    fn simulation_stops_with_events_still_in_queue() {
+        let mut sim = setup();
+        sim.event_queue.schedule_from_boxed(Box::new(CompletionEvent {}), SimTime { time: 3 }).unwrap();
+        sim.run();
+
+        let expected = vec![1, 3];
+        assert_eq!(expected, sim.state.executed_event_values, "simulation did not terminate with completion event");
     }
 }
