c221f9f3456929df41aa062024464a95014b5fe4
Bill Pace <billpace24@gmail.com>
2025-02-15 11:49:10 -0600

    add description for serial mod

--- src/serial.rs
+++ src/serial.rs
@@ -1,3 +1,16 @@
+//! This module provides the capability to define a
+//! discrete-event simulation that executes entirely
+//! from one thread. This module enforces minimal
+//! requirements on client code and expects that only
+//! one thread will directly interact with the
+//! simulation's event queue and overall state.
+//! 
+//! As a result, simulations built from these pieces
+//! may consume fewer resources at runtime than
+//! simulations built from the [`threadsafe`] module.
+//! 
+//! [`threadsafe`]: crate::threadsafe
+
 mod events;
 mod simulation;
 
